00c08a18a4d2c0358f0246f5d79ae4b04bf9375b
thumaraujo <165036906+thumaraujo@users.noreply.github.com>
2024-03-29 17:05:26 +0000

    added the API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = temperature;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+}
+function searchCity(city) {
+  let apiKey = "406tac3d1af0db7dfbb34a6c42bb8ofe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("London");
